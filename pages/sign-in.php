da16134b3ba772a5a2a6b8913c95fac64ba46c6a
khalid fifel <fifle.khalid99@gmail.com>
2022-11-10 23:50:14 +0100

    add log in page wit the css and html needed

---
+++ pages/sign-in.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/log-in.css">
+    <title>Log in</title>
+</head>
+<body>
+    <main>
+        <div class="d-flex">
+            <div  class="card col-6  w-100 p-3 py-4 ">
+                <h1 class="text-center">
+                    <span>Log</span>
+                    <span class="text-primary">in</span>
+                </h1>
+                <div class="text-center mt-5">
+                    <form action="login.php" method="post" class="d-grid gap-5">
+                            <div>
+                                <input type="email" name="email" placeholder="Email" class="form-control">
+                            </div>
+                            <div>
+                                <input type="password" name="password" placeholder="mot de passe" class="form-control">
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <input type="submit" value="Connexion" class="btn btn-primary">
+                                <span><a href="#">Récuperer votre mot de passe</a></span>
+                            </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-6  w-100">
+                <h1>YOU<span class="text-primary">BOOK</span></h1>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
