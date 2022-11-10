a12021946c529beae7b62d3d0b5b6f38cc753d70
khalid fifel <fifle.khalid99@gmail.com>
2022-11-10 09:10:52 +0100

    add side bar

---
+++ include/sidebar.php
@@ -0,0 +1,38 @@
+<div class=" col-1 sideBar">
+    <div class="row justify-items-center justify-content-center gap-3">
+        <a href="#">
+            <lord-icon
+                    src="https://cdn.lordicon.com/osuxyevn.json"
+                    trigger="hover"
+                    colors="primary:#FFFFFF"
+                    class="logo">
+            </lord-icon>
+        </a>
+        <a href="">
+            <lord-icon
+                    src="https://cdn.lordicon.com/kipaqhoz.json"
+                    trigger="morph"
+                    class="fa-lg"
+                    colors="primary:#FFFFFF"
+                    class="logo"
+            >
+            </lord-icon>
+        </a>
+        <a href="">
+            <lord-icon
+                    src="https://cdn.lordicon.com/ofwpzftr.json"
+                    trigger="hover"
+                    colors="primary:#FFFFFF"
+                    class="logo">
+            </lord-icon>
+        </a>
+        <a href="">
+            <lord-icon
+                    src="https://cdn.lordicon.com/hwuyodym.json"
+                    trigger="hover"
+                    colors="primary:#FFFFFF"
+                    class="logo">
+            </lord-icon>
+        </a>
+    </div>
+</div>
