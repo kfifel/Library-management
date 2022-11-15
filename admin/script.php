25dad2eed249b1eadf13e0135c80592cfa056e94
khalid fifel <fifle.khalid99@gmail.com>
2022-11-15 11:46:52 +0100

    create the function of validate the string and add the function logUp

--- admin/script.php
+++ admin/script.php
@@ -2,16 +2,23 @@
     include('database/dbConnexion.php');
     session_start();
 
-    if(isset($_POST['connexion'])) {
-        $email = stripslashes($_POST['email']);
+    if(isset($_POST['log-in']))  login();
+    if(isset($_POST['log-up'])) logUp();
+
+
+
+
+    function login(){
         global $conn;
-        $email = mysqli_real_escape_string($conn, $email);
-        $password = stripslashes($_POST['password']);
-        $password = mysqli_real_escape_string($conn, $password);
+
+        $email = vierifyString($_POST['email']);
+        $password = vierifyString($_POST['password']);
+
         $query = "SELECT * FROM manager WHERE email = '$email' and password='" . hash('sha256', $password) . "'";
         $result = mysqli_query($conn, $query);
         $rows = mysqli_fetch_assoc($result);
-        if ( mysqli_num_rows($result) > 1) {
+
+        if ( mysqli_num_rows($result) > 0) {
             $_SESSION['id']  = $rows['id'];
             $_SESSION['first_name_admin'] = $rows['first_name'];
             $_SESSION['last_name_admin']  = $rows['last_name'];
@@ -20,6 +27,37 @@
         } else {
             $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
             echo $message;
-            header("Location: /pages/sign-in.php");
+            header("Location: /pages/login.php");
         }
+    }
+
+    function logUp(){
+        if(!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['first_name']) && !empty($_POST['last_name'])){
+            global $conn;
+            $email = vierifyString($_POST['email']);
+            $password = vierifyString($_POST['password']);
+            $first_name = vierifyString($_POST['first_name']);
+            $last_name = vierifyString($_POST['last_name']);
+
+            $req = "INSERT INTO manager VALUE (null, $first_name, $last_name, $email,".hash('sha256', $password).")";
+            $res = mysqli_query($conn, $req);
+            if($res){
+                $_SESSION['message'] = "bien enregistrer ";
+                header('Location: /index.php');
+            }
+            else{
+                $_SESSION['message'] = "erreur d'enregistrement :".mysqli_error();
+                header('Location: /index.php');
+            }
+        }else{
+            $_SESSION['message'] = "il faut remplir touts les champs ";
+            header('Location: /index.php');
+        }
+
+    }
+
+    function vierifyString($str): string{
+        global $conn;
+        $str = stripslashes($str);
+        return mysqli_real_escape_string($conn, $str);
     }
