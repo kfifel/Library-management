b15c3a3cab2c5c495ff46221163aebbaaed7f71d
khalid fifel <fifle.khalid99@gmail.com>
2022-11-22 16:24:33 +0100

    styling add admin page

--- pages/add-admins.php
+++ pages/add-admins.php
@@ -12,58 +12,88 @@ if(!isset($_SESSION['id_admin'])){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/css/log-in.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <title>Log in</title>
 </head>
-<body class="bodyAddAdmin">
+<body>
+
+<?php  include '../include/header.php'; ?>
 <main>
-    <div class="d-flex">
-        <div  class="card col-5 p-3 py-2 align-content-center" id="addAdmin">
-            <div class="card-body row justify-content-center">
-                <h1 class="text-center">
-                    <span class="text-white">Ajouter</span>
-                    <span class="text-primary">Admin</span>
-                </h1>
-                <div class="text-center mt-5">
-                    <form action="/admin/script.php" method="post" class="d-grid gap-5">
-                        <div class="row">
-                            <div class="col-6">
-                                <input type="text" name="first_name" id="first_name" placeholder="Prénom" class="form-control">
+    <div class="main-container d-flex">
+        <div class="mt-10 col-1" id="side_bar">
+            <?php include '../include/sidebar.php' ?>
+        </div>
+        <div class=" col-11 ml-8 mt-10">
+            <div class="card-header p-3 text-white">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item">livres</li>
+                    <li class="breadcrumb-item breadcrumb-active">Apreçu</li>
+                </ul>
+                <div class="d-flex justify-content-center" id="header-title">
+                    <h1> ajouter admin</h1>
+                </div>
+            </div>
+            <div class="card-body px-5 d-flex justify-content-center">
+                <div class="col-7">
+                    <div class="">
+                        <?php if(!empty($_SESSION['message'])):?>
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                <b>Success: </b><?=$_SESSION['message']?>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                             </div>
-                            <div class="col-6">
-                                <input type="text" name="last_name" id="last_name" placeholder="Nom" class="form-control">
+                        <?php
+                            unset($_SESSION['message']);
+                            endif;
+                        if(!empty($_SESSION['error'])): ?>
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <b>Ereur: </b><?=$_SESSION['error']?>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        <?php
+                            unset($_SESSION['error']);
+                            endif;
+                        ?>
+                    </div>
+                    <form action="/admin/script.php" method="post">
+                        <div class="d-grid gap-5">
+                            <div class="row">
+                                <div class="col-6">
+                                    <input type="text" name="first_name" id="first_name" value="<?=@$_SESSION['first_name']; unset($_SESSION['first_name']);?>" placeholder="Prénom" class="form-control">
+                                </div>
+                                <div class="col-6">
+                                    <input type="text" name="last_name" id="last_name" value="<?=@$_SESSION['last_name']; unset($_SESSION['last_name']);?>"  placeholder="Nom" class="form-control">
+                                </div>
+                            </div>
+                            <div>
+                                <input type="email" name="email" id="email" value="<?=@$_SESSION['emailNv']; unset($_SESSION['emailNv']);?>"  placeholder="Email" onkeyup="emailValidate()" class="form-control">
+                            </div>
+                            <div>
+                                <input type="password" name="password" id="password" placeholder="mot de passe" onkeyup="passwordValidate()" class="form-control">
+                            </div>
+                            <div>
+                                <input type="password" name="password2" id="password2" placeholder="mot de passe" class="form-control">
                             </div>
                         </div>
-                        <div>
-                            <input type="email" name="email" id="email" placeholder="Email" onkeyup="emailValidate()" class="form-control">
-                        </div>
-                        <div>
-                            <input type="password" name="password" id="password" placeholder="mot de passe" onkeyup="passwordValidate()" class="form-control">
-                        </div>
-                        <div>
-                            <input type="password" name="password2" id="password2" placeholder="mot de passe" class="form-control">
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <input type="submit" value="Connexion" name="add-admin" class="btn btn-primary">
-                            <span><a href="#">Récuperer votre mot de passe</a></span>
+                        <div class="d-flex justify-content-between my-4">
+                            <input type="submit" value="Ajouter" name="add-admin" class="btn btn-primary form-control">
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-        <div class="text-center d-grid justify-items-center  title">
-            <a href="/index.php" class="text-decoration-none">
-                <h1 class=" my-5">
-                    <span class="text-muted">YOU</span>
-                    <span class="text-primary">BOOK</span>
-                </h1>
-            </a>
-            <div class="mt-5">
-                <img src="../assets/images/sigup.png" id="logup" alt="add admins up">
-            </div>
-        </div>
     </div>
 </main>
-<script src="../assets/js/main.js"></script>
+<footer>
+    <?php
+    include '../include/footer.php';
+    ?>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://cdn.lordicon.com/qjzruarw.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
